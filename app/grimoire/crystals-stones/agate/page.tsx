b38b7734418f99e93096d57299cc84197d1b1d62
Wicked <wickedwitchsweb@gmail.com>
2026-04-03 12:39:55 -0500

    remove grimoire from app for build

--- app/grimoire/crystals-stones/agate/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PlaceholderPage() {
-  return (
-    <main style={{ padding: "4rem", textAlign: "center" }}>
-      <h1>Under Construction</h1>
-      <p>This page is being written.</p>
-    </main>
-  );
-}
